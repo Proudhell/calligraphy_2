38080275e605309983ac060535add7f250e491c6
Proudhell <Proudhell@gmail.com>
2022-07-07 08:45:03 +0300

    navbar_v3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,23 @@
 import React from 'react'
 
 export default function Header() {
-    return (
-        <ul>
-            <ul class="navbar_links">
-                <li><a class="navbar-links" href="teasers">teasers</a></li>
-                <li><a class="navbar-links" href="constructor">constructor</a></li>
-                <li><a class="navbar-links" href="manufacturing">manufacturing</a></li>
-            </ul>
-            <ul class="navbar_links">
-                <li><a class="navbar-links" href="catalog">catalog</a></li>
-                <li><a class="navbar-links" href="discounts">discounts</a></li>
-                <li><a class="navbar-links" href="about_author">about author</a></li>
-            </ul>
+    return (      
+        <ul >
+            <div class="left-buttons">
+                <ul class="navbar_links">
+                    <li><a class="navbar-links" href="teasers">teasers</a></li>
+                    <li><a class="navbar-links" href="constructor">constructor</a></li>
+                    <li><a class="navbar-links" href="manufacturing">manufacturing</a></li>
+                </ul>
+                <ul class="navbar_links">
+                    <li><a class="navbar-links" href="catalog">catalog</a></li>
+                    <li><a class="navbar-links" href="discounts">discounts</a></li>
+                    <li><a class="navbar-links" href="about_author">about author</a></li>
+                </ul>
+            </div>
             <li class="navbar_link_right">
                 <a href="shopping_cart">shopping cart 5 &gt;</a>
-                </li>
+            </li>
         </ul>
     )
 }
---
+++ src/components/Page1.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Page1() {
+    return (
+        <div>
+            <div class="pic1 pic" >
+            </div>
+            <div class="pic2 pic">
+            </div>
+            <div class="pic3 pic">
+            </div>
+            <div class="pic4 pic">
+            </div>
+            <div class="pic5 pic" >
+            </div>
+            <div class="pic6 pic">
+            </div>
+            <div class="pic7 pic">
+            </div>
+            <div class="pic8 pic">
+            </div>
+        </div>
+    )
+}
