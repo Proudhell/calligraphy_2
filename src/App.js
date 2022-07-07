2b81d0532c841e803f283ea3500d58849d665841
Proudhell <Proudhell@gmail.com>
2022-07-07 08:44:40 +0300

    navbar_v3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Page1 from "./components/Page1"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Background from "./components/Background";
 
 function App() {
   return (
     <body>
       <div>
-        <Header />
+      <Header />
+        <Page1/>
+        
         <div class="awfw">
         </div>
         <Footer />
