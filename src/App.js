c45674610e4d0f6a1cb29cf894a9fbb3739f72a2
Proudhell <Proudhell@gmail.com>
2022-07-11 17:02:37 +0300

    background first page v1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Page1 from "./components/Page1"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Background from "./components/Background";
 
 function App() {
   return (
     <body>
       <div>
-      <Header />
-        <Page1/>
+        <Header/>
+        <Background/>
         
         <div class="awfw">
         </div>
