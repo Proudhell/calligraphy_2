8d60237c6ce68a49b7466aaf239cb801d4da6f6e
PRAGMA-PC-20 <sammeln1719@gmail.com>
2022-07-12 16:53:58 +0300

    Зафиксировал глэка

--- src/mainPage/Background.jsx
+++ src/mainPage/Background.jsx
@@ -3,15 +3,21 @@ import logo from './img/logo.svg';
 import styles from './style/mainpage.module.scss';
 
 const Background = function() {
-
-    
+    // const [iconsBanner, setIconsBanner] = useState(0);
+    // const imageContainer = 308;
 
     const windowInnerWidth = document.documentElement.clientWidth;
     const windowInnerHeight = document.documentElement.clientHeight;
 
     console.log(windowInnerWidth);
     console.log(windowInnerHeight);
-
+    
+    const fixedWindow = {
+         width: `100vw`,
+         height: `100vh`,
+        // width: `${windowInnerWidth}px`,
+        // height: `${windowInnerHeight}px`,
+    }
 
     return (
         <div className={styles.wrapperBanner} >
