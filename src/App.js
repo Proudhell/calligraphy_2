9f41ed35b1c2c4a9828f5865ae5aa31b8d624cf8
Misha <proudhell@gmail.com>
2022-07-14 22:10:09 +0300

    тест

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Topbar from "./components/Topbar"
 import Page1 from "./components/Page1"
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import Background from "./mainPage/Background";
-
+//тест коммит
 function App() {
   return (
     <body>
